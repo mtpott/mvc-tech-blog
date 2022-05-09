278081d51d179a11e7fe4960b305a0068620beab
megan <mtpott23@gmail.com>
2022-05-09 18:51:30 -0400

    add data to seed, handlebars functionality

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -2,4 +2,67 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User } = require('../models');
 
-//get all blog posts on home page
+//get all blog posts on home page and see the title and the date created
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts,
+            loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+//get one blog post and see the title, contents, post creator's username, and date created for that post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'no post found with this id.' });
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+        res.render('single-post', { post, 
+        loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//login information
+//if the user is logged in, redirect to "/". if not, render the login.handlebars page
---
+++ seeds/user.js
@@ -0,0 +1,19 @@
+const { User } = require('../models/User');
+
+const userData = [
+    {
+        username: 'user_1',
+        email: 'user_1@email.com',
+        password: 'password1234'
+    },
+    {
+        username: 'user_2',
+        email: 'user_2@email.com',
+        password: 'password9876'
+    },
+    {
+        username: 'user_3',
+        email: 'user_3@email.com',
+        password: 'password2468'
+    }
+]
